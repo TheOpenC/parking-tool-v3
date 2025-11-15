f7efa5e7b7bfc893e846283b5a755d1aac81899d
Drew Dudak <ddudak@gmail.com>
2025-11-14 21:46:40 -0500

    Initial clean commit for parking-tool-v3

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+let today = new Date();
+let month = String(today.getMonth()+1).padStart(2, '0'); //MM
+let day = String(today.getDate()).padStart(2, '0'); //DD
+let year = String(today.getFullYear()); //YYYY
+
+let parkingUrl = `https://api.nyc.gov/public/api/GetCalendar?fromdate=${month}%2F${day}%2F${year}&todate=${+month + 1}%2F${day}%2F${year}`
+
+fetch(parkingUrl, {
+    method: 'GET',
+    // REQUEST HEADERS
+    headers: {
+        'Cache-Control': 'no-cache',
+        'Ocp-Apim-Subscription-Key': process.env.API_KEY}
+    })
+
+
+    .then(data => {
+        console.log(data)
+        console.log(data.text())
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
