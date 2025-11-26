b2be1baf63e9d8bdac816e088086fa0bc643baf4
Drew Dudak <ddudak@gmail.com>
2025-11-26 12:45:56 -0500

    testing a dummy report for the scheduler

--- netlify/functions/parking-scheduler.mjs
+++ netlify/functions/parking-scheduler.mjs
@@ -1,7 +1,24 @@
+import { getStore } from "@netlify/blobs";
+
 export default async (req) => {
     const { next_run } = await req.json()
 
-    console.log("Received event! Next invocation at:", next_run)
+    // 1) get reference to a blob store
+    const store = getStore('parking-reports')
+
+    // 2) build a simple test report
+    const now = new Date().toISOString();
+    const reportText = [
+        'ASipP test report',
+        `Generated at: ${now}`,
+        `Next scheduled run: ${next_run}`,
+        "", 
+    ].join("\n");
+
+    // 3) write to the 'latest' key
+    await store.set('latest', reportText);
+
+    console.log(`Parking-scheduler: wrote report at time: ${now}. Next run @ `, next_run)
 }
 
 // tell netlify when to run 
