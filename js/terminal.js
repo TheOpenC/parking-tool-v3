a0cff7071021cc186fa5221e4358124fcb002016
Drew Dudak <ddudak@gmail.com>
2026-01-22 22:35:03 -0500

    comments

--- js/terminal.js
+++ js/terminal.js
@@ -153,7 +153,7 @@ Fetching report `;
 function fetchAndShowReport() {
   const REPORT_URL = '/.netlify/functions/report'
 
-  // Adding return. Original did not have Return
+  // Adding return. Original did not have Returnn
 
   return fetch(REPORT_URL)
     .then((response) => response.text())
